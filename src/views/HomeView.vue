3f29451b6d891041392a0af553a7c76c69fd612e
iamabhijeet2003 <abhijeet3016@gmail.com>
2024-06-26 21:01:43 +0200

    create homeview

---
+++ src/views/HomeView.vue
@@ -0,0 +1,19 @@
+<template>
+    <h1>BMI Calculator</h1>
+    <div>
+        <h2>Metric Units</h2>
+        <div>
+            <MetricUnits></MetricUnits>
+        </div>
+    </div>
+</template>
+
+<script>
+import MetricUnits from "../components/forms/MetricUnits.vue";
+
+export default{
+    components:{
+        MetricUnits
+    }
+}
+</script>
